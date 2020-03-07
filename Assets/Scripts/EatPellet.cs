2457a88092f958a1e046c15ccc79c15922a6c859
Patrick <prm5983@rit.edu>
2020-03-07 16:05:25 -0500

    Tried to implement segments following player, failed

--- Assets/Scripts/EatPellet.cs
+++ Assets/Scripts/EatPellet.cs
@@ -1,11 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EatPellet : MonoBehaviour
 {
-    void Update()
+    [SerializeField]
+    ///How many pellets are needed to finish the level.
+    private int pelletsToEat;
+    private int pelletsEaten = 0;
+
+    [SerializeField]
+    private float spawnRange;
+    [SerializeField]
+    private GameObject player;
+
+    private void Start()
     {
-        
+        RelocatePellet();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Increase the number of pellets eaten.
+            pelletsEaten++;
+
+            //If enough pellets are eaten, end. Otherwise, relocate it.
+            if (pelletsEaten >= pelletsToEat)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                RelocatePellet();
+            }
+        }
+    }
+
+    public void RelocatePellet()
+    {
+        //Set up a new position and make sure it's never too close to the player.
+        Vector3 newPos = player.transform.position;
+        while (Vector3.Distance(newPos, player.transform.position) <= 1)
+        {
+            newPos = new Vector3
+            (
+                Random.Range(-spawnRange, spawnRange),
+                0.5f,
+                Random.Range(-spawnRange, spawnRange)
+            );
+        }
+
+        //Then set this pellet's transform to that new position.
+        transform.position = newPos;
     }
 }
---
+++ Assets/Scripts/SegmentFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private Vector3 offset;
+
+    [SerializeField]
+    private int orderInSnake;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3
+            (
+                player.transform.position.x + offset.x,
+                player.transform.position.y + offset.y,
+                player.transform.position.z + offset.z
+            );
+
+        transform.LookAt(player.transform);
+    }
+}
